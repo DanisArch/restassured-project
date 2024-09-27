e9c6f097e66c579014ae96155cb28bae5f98315a
Denys Nazarov <div.itcor@gmail.com>
2024-09-27 12:52:25 +0200

    add getTestUserProfile and update config.properties

--- src/test/java/tests/ProfileTests.java
+++ src/test/java/tests/ProfileTests.java
@@ -1,4 +1,28 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+import utils.Config;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static utils.Config.getConfig;
+import static wrappers.ApiWrapper.sendGetRequest;
+
 public class ProfileTests {
+
+    @Test
+    public void getTestUserProfile() {
+         sendGetRequest(
+                given().headers("x-auth-token", Config.getConfig("token")),
+                getConfig("baseURI")
+                        + getConfig("objectProfileMePath"))
+                .assertThat()
+                .body("company", equalTo(Config.getConfig("company")))
+                .body("location", equalTo(Config.getConfig("location")))
+                .body("status", equalTo(Config.getConfig("status")))
+                .body("profileId",equalTo(Config.getConfig("profilId"))
+                );
+    }
+
+
 }
--- src/test/java/tests/UserTests.java
+++ src/test/java/tests/UserTests.java
@@ -80,11 +80,10 @@ public class UserTests extends ApiBaseTest {
         );
 
         assertNotNull(responseAuth.getToken());
-
-/*        // Сравниваем полученный токен с тем, что у нас есть
+        //TODO: Создать environment, где будут хранится переменные token
+/*      // И добавить тест по сравнению полученного токена с тем, что у нас есть
         String expectedToken = Config.getConfig("token");
         String actualToken = responseAuth.getToken();  // Предполагается, что есть метод getToken в классе AuthResponse
-
         assertEquals(expectedToken, actualToken, "Токены не совпадают!");*/
     }
 
@@ -98,45 +97,4 @@ public class UserTests extends ApiBaseTest {
                 .assertThat()
                 .body(matchesJsonSchemaInClasspath("user-schema.json"));
     }
-
-
-/*
-//Этот тест скопировать для получения профиля по Id
-    private ApiWrapper apiWrapper = new ApiWrapper();
-    @Test
-    public void testGetUserById() {
-        Response response = apiWrapper.getUserById(50);
-        assertEquals(200, response.getStatusCode());
-
-        User user = response.as(User.class);
-        assertNotNull(user);
-        assertEquals(50, user.getId());
-    }
-
-    @Test
-    public void testRegisterUser() {
-        User newUser = new User();
-        newUser.setName("John Doe");
-        newUser.setEmail("john@example.com");
-
-        Response response = apiWrapper.createUser(newUser);
-        assertEquals(201, response.getStatusCode());
-
-        User createdUser = response.as(User.class);
-        assertEquals("John Doe", createdUser.getName());
-    }
-
-    @Test
-    public void testLoginAsUser() {*/
-        /*
-        UserDTO newUser = new UserDTO();
-        newUser.setName("John Doe");
-        newUser.setEmail("john@example.com");
-
-        Response response = apiWrapper.createUser(newUser);
-        assertEquals(201, response.getStatusCode());
-
-        UserDTO createdUser = response.as(UserDTO.class);
-        assertEquals("John Doe", createdUser.getName());
-    }*/
 }
